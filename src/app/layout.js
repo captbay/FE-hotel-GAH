0ba2a0f83f9c6db40e125d222642bac6628ccfbf
I Putu Agestya Pramana <agespramana9@gmail.com>
2023-10-30 13:11:30 +0700

    setting dark

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" class="dark">
       <body className={outfitOutfit.className}>
         <ToastContainer />
         {children}
